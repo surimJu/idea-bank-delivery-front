fa93dc8402543d65d245bc8c48a730620167f11b
주수림 <mz_surimju@woowafriends.com>
2022-05-27 15:59:19 +0900

    config added

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,9 @@
 module.exports = {
-  transpileDependencies: ["vuetify"],
+	transpileDependencies: ['vuetify'],
+	outputDir: './dist/build',
+	assetsDir: './static/',
+	devServer: {
+		proxy: 'http://localhost:1212',
+		port: 2121,
+	},
 };
